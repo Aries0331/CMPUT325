8e7559024ae21f717bd746a18c15dfc7046b3f22
Aries0331 <jinzhu@ualberta.ca>
2017-01-23 23:49:58 -0700

    testing q1

--- Assignment1/1461696.lisp
+++ Assignment1/1461696.lisp
@@ -1,8 +1,20 @@
+;Name: Jinzhu Li
+;Student ID: 1461496
+;Course: CMPUT 325 B1
+;Assignment 1
+
 #| QUESTION 1
 The function xmember takes two elements X and Y. X must be a list, Y can be either a list or an atom.
 If Y is a member of X, returns T otherwise returns NIL.
 |#
+
 (defun xmember (X Y)
+  (cond
+    ((null Y) NIL)
+    ((equal (car X) Y) T)
+    (t (xmember (cdr X) Y))
+  )
+)
 
 
 
