6f1e8ffb63883231a36fded4b42b0d217c911f76
Aries0331 <jinzhu@ualberta.ca>
2017-01-23 22:58:49 -0700

    begin as1

---
+++ Assignment1/1461696.lisp
@@ -0,0 +1,17 @@
+#| QUESTION 1
+The function xmember takes two elements X and Y. X must be a list, Y can be either a list or an atom.
+If Y is a member of X, returns T otherwise returns NIL.
+|#
+(defun xmember (X Y)
+
+
+
+;QUESTION 2
+
+;QUESTION 3
+
+;QUESTION 4
+
+;QUESTION 5
+
+;QUESTION 6
