129558b887f93a2d71c5619fb10289af9c0283ff
Aries0331 <jinzhu@ualberta.ca>
2017-01-24 21:35:47 -0700

    fix q1 and finish q2, q3

--- Assignment1/1461696.lisp
+++ Assignment1/1461696.lisp
@@ -10,17 +10,34 @@ If Y is a member of X, returns T otherwise returns NIL.
 
 (defun xmember (X Y)
   (cond
-    ((null Y) NIL)
+    ((NULL X) NIL)
     ((equal (car X) Y) T)
     (t (xmember (cdr X) Y))
   )
 )
 
 
-
 ;QUESTION 2
 
+(defun flatten (X)
+  (cond
+    ((NULL X) NIL)
+    ((atom (car X)) (append (list (car X)) (flatten (cdr X))))
+    (t (append (flatten (car X)) (flatten (cdr X))))
+  )
+)
+
+
 ;QUESTION 3
+
+(defun mix (L2 L1)
+  (cond
+    ((NULL L1) L2)
+    ((NULL L2) L1)
+    (t (append (list (car L1) (car L2)) (mix (cdr L2) (cdr L1))))
+  )
+)
+
 
 ;QUESTION 4
 
