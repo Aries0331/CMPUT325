0ef77cf5dc234e6d8ef327a1f1c992286feaae29
Aries0331 <jinzhu@ualberta.ca>
2017-02-21 16:32:14 -0700

    half waydone

--- Assignment2/1461696.lisp
+++ Assignment2/1461696.lisp
@@ -3,6 +3,9 @@
 ;Course: CMPUT 325 B1
 ;Assignment 2
 
+#|
+The main interpreter function
+|#
 (defun fl-interp (E P)
   (cond 
 	((atom E) E)   ;this includes the case where expr is nil
@@ -28,17 +31,18 @@
 	        	((eq f 'and)  (if (and (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
 	        	((eq f 'or)  (if (or (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
 	        	((eq f 'not)  (if (not (fl-interp (car arg) P) (cadr (cadr arg) P)) T nil))
-
 	        	; .....
+
 	            ; if f is a user-defined function,
                 ;    then evaluate the arguments 
                 ;         and apply f to the evaluated arguments 
                 ;             (applicative order reduction) 
-                
+                ; .....
+
 
                 ; otherwise f is undefined; in this case,
                 ; E is returned as if it is quoted in lisp
-                ; .....
+
                 (t E)
                 )
 	      )
@@ -47,3 +51,50 @@
   )
 
 
+#|
+Get the number of parameters in the function
+Example: 
+(countArg '(f x = (+ x 2))) -> 1
+(countArg '(f x y = (+ x y))) -> 2
+|#
+(defun countArg (L)
+	(cond
+		((NULL (cdr L)) 0)
+		((equal '= (car L)) -1)
+		(t (+ 1 (countArg (cdr L))))
+	)
+)
+
+#|
+the function takes into two arguments, a list and one of the element in the list
+return the index of given element in the list
+the index is range from 0 to len(list)-1
+--Assume the element must be one of the elements in the list L
+Example:
+(getIndex '(a b c) 'a) -> 0
+(getIndex '(a b c) 'b) -> 1
+|#
+(defun getIndex (L x)
+	(cond
+		((NULL L) 0)
+		((eq (car L) x) 0)
+		(t (+ 1 (getIndex (cdr L) x)))
+	)
+)
+
+#|
+the function takes into two arguments, a list and an index
+returns an element which is at the position of index+1
+since the index is range from 0 to len(L)-1
+--Assume the index n is always valid
+Example:
+(getElement '(a b c) '2) -> c
+(getElement '(a b c) '1) -> b
+|#
+(defun getElement (L n)
+	(cond
+		((NULL L) 0)
+		((eq n 0) (car L))
+		(t (getElement (cdr L) (- n 1)))
+	)
+)
