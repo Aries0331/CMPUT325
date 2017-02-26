c3b08b3035812e9ebd23c46142efed943d1ab027
Aries0331 <jinzhu@ualberta.ca>
2017-02-25 20:33:39 -0700

    add test

--- Assignment2/1461696.lisp
+++ Assignment2/1461696.lisp
@@ -6,55 +6,6 @@
 
 
 #|
-The main interpreter function
-|#
-(defun fl-interp (E P)
-  (cond 
-	((atom E) E)   ;this includes the case where expr is nil
-        (t
-           (let ( (f (car E))  (arg (cdr E)) )
-	      (cond 
-                ; handle built-in functions
-                ((eq f 'if)  (if (fl-interp (car arg) P) (fl-interp (cadr arg) P) (fl-interp (caddr arg) P)))
-                ((eq f 'null)  (null (fl-interp (car arg) P)))
-                ((eq f 'atom)  (atom (fl-interp (car arg) P)))
-                ((eq f 'eq)  (eq (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
-                ((eq f 'first)  (car (fl-interp (car arg) P)))
-                ((eq f 'rest)  (cdr (fl-interp (car arg) P))) 
-	        	((eq f 'cons)  (cons (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
-	        	((eq f 'equal)  (equal (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
-	        	((eq f 'isnumber)  (numberp (fl-interp (car arg) P)))
-	        	((eq f '+)  (+ (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
-	        	((eq f '-)  (- (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
-	        	((eq f '*)  (* (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
-	        	((eq f '>)  (if (> (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
-	        	((eq f '<)  (if (< (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
-	        	((eq f '=)  (if (= (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
-	        	((eq f 'and)  (if (and (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
-	        	((eq f 'or)  (if (or (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
-	        	((eq f 'not)  (if (not (fl-interp (car arg) P)) T nil))
-	        	; .....
-
-	            ; if f is a user-defined function,
-                ;    then evaluate the arguments 
-                ;         and apply f to the evaluated arguments 
-                ;             (applicative order reduction) 
-                ; .....
-
-                ((isUD f P) (fl-interp (subs (getVar (cdar P)) (getBody (cdar P)) arg) nil))
-
-                ; otherwise f is undefined; in this case,
-                ; E is returned as if it is quoted in lisp
-
-                (t E)
-
-                )
-	      )
-           )
-        )
-  )
-
-#|
 Example:
 (getVar '(x y = (+ x y))) -> (x y)
 (getVar '(x = (* x x))) -> (x)
@@ -181,7 +132,54 @@ Example:
 	)
 )
 
+#|
+The main interpreter function
+|#
+(defun fl-interp (E P)
+  (cond 
+	((atom E) E)   ;this includes the case where expr is nil
+        (t
+           (let ( (f (car E))  (arg (cdr E)) )
+	      (cond 
+                ; handle built-in functions
+                ((eq f 'if)  (if (fl-interp (car arg) P) (fl-interp (cadr arg) P) (fl-interp (caddr arg) P)))
+                ((eq f 'null)  (null (fl-interp (car arg) P)))
+                ((eq f 'atom)  (atom (fl-interp (car arg) P)))
+                ((eq f 'eq)  (eq (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+                ((eq f 'first)  (car (fl-interp (car arg) P)))
+                ((eq f 'rest)  (cdr (fl-interp (car arg) P))) 
+	        	((eq f 'cons)  (cons (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+	        	((eq f 'equal)  (equal (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+	        	((eq f 'isnumber)  (numberp (fl-interp (car arg) P)))
+	        	((eq f '+)  (+ (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+	        	((eq f '-)  (- (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+	        	((eq f '*)  (* (fl-interp (car arg) P) (fl-interp (cadr arg) P)))
+	        	((eq f '>)  (if (> (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
+	        	((eq f '<)  (if (< (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
+	        	((eq f '=)  (if (= (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
+	        	((eq f 'and)  (if (and (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
+	        	((eq f 'or)  (if (or (fl-interp (car arg) P) (fl-interp (cadr arg) P)) T nil))
+	        	((eq f 'not)  (if (not (fl-interp (car arg) P)) T nil))
+	        	; .....
 
+	            ; if f is a user-defined function,
+                ;    then evaluate the arguments 
+                ;         and apply f to the evaluated arguments 
+                ;             (applicative order reduction) 
+                ; .....
+
+                ((isUD f P) (fl-interp (subs (getVar (cdar P)) (getBody (cdar P)) arg) P))
+
+                ; otherwise f is undefined; in this case,
+                ; E is returned as if it is quoted in lisp
+
+                (t E)
+
+                )
+	      )
+           )
+        )
+  )
 
 
 
