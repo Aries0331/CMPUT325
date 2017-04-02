0af8c40a2d4a1d65fb9d81db8c30623c30104592
Aries0331 <jinzhu@ualberta.ca>
2017-04-01 18:18:55 -0600

    as4

---
+++ Assignment4/1461696.pl
@@ -0,0 +1,18 @@
+% Question1 - Four squares
+% fourSquares(+N, [-S1, -S2, -S3, -S4])
+% S1 <= S2 <= S3 <= S4
+% On backtracking, fourSquares should return all possible answers 
+% since there may be more than one solution
+
+fourSquares(N, [S1, S2, S3, S4]) :- 
+
+
+
+
+
+% Question 2 - War and Peace
+% the total strength of one month's dismantlement is less than or equal to 
+% the total strength of next month's dismantlement
+% If there is no solution, then the program should return false
+
+disarm(+Adivisions, +Bdivisions,-Solution)
