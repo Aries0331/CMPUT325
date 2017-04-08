8a90a8dfaa30c8365b8226b536a46d5336a311a1
Aries0331 <jinzhu@ualberta.ca>
2017-04-08 12:59:19 -0600

    finish as4, need comments

--- Assignment4/1461696.pl
+++ Assignment4/1461696.pl
@@ -8,10 +8,8 @@
 % since there may be more than one solution
 
 fourSquares(N, [S1, S2, S3, S4]) :- 
-	S1 #>= 0,
-	S2 #>= 0,
-	S3 #>= 0,
-	S4 #>= 0,
+	Vars = [S1, S2, S3, S4],
+	Vars ins 0..N,
 	S1*S1 + S2*S2 + S3*S3 + S4*S4 #= N,
 	S2 #>= S1,
 	S3 #>= S2,
@@ -26,4 +24,32 @@ fourSquares(N, [S1, S2, S3, S4]) :-
 % the total strength of next month's dismantlement
 % If there is no solution, then the program should return false
 
-%disarm(+Adivisions, +Bdivisions,-Solution) :- 
+disarm(Adivisions, Bdivisions, Solution) :- subdisarm(Adivisions, Bdivisions, Solution, 0).
+subdisarm([],[],[],_).
+subdisarm(Adivisions, Bdivisions, Solution, Pre) :-
+	Vars = [A1, A2, B],
+	select(A1, Adivisions, Temp),
+	select(A2, Temp, ResultA),
+	select(B, Bdivisions, ResultB),
+	A1 #=< A2,
+	A1+A2 #= B,
+	B #>= Pre,
+	subdisarm(ResultA, ResultB, Subsolution, B), !,
+	label(Vars),
+	append([[[A1, A2], [B]]], Subsolution, Solution).
+
+subdisarm(Adivisions, Bdivisions, Solution, Pre) :-
+	Vars = [A, B1, B2],
+	select(B1, Adivisions, Temp),
+	select(B2, Temp, ResultB),
+	select(A, Bdivisions, ResultA),
+	B1 #=< B2,
+	B1+B2 #= A,
+	A #>= Pre,
+	subdisarm(ResultA, ResultB, Subsolution, A), !,
+	label(Vars),
+	append([[[B1, B2], [A]]], Subsolution, Solution).
+
+
+
+
